97634922a42b27b7e898d665efc1a611b61b69b5
EmimalGnanaMerciba <emi.pauldurai@gmail.com>
2022-11-25 11:59:20 +0530

    sam code added

--- src/vote/VoteRule.java
+++ src/vote/VoteRule.java
@@ -3,6 +3,7 @@ package vote;
 public class VoteRule {
 public static void main(String[] args) {
 	int age=10;
+	System.out.println("Age is:"+age);
 			
 	if(age >= 18) {
 		System.out.println("Input:"+age);
